f808680e0aaa677eeb8420758307889d9422f2d2
amdomgere <amdomgere@gmail.com>
2022-07-05 22:29:33 +0300

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value of int
+ *
+ * @a: as int
+ *
+ * Return: abs value
+ */
+
+
+
+int _abs(int)
+{
+
+	if (a >= 0)
+	{
+		return (a);
+	}
+	else
+	{
+		return (a * -1);
+	}
+
+}
+
+
+
