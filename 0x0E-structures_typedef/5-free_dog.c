d5e61eea04d41f328d8fc40ed3aaf1ed6cd00379
amdomgere <106970630+amdomgere@users.noreply.github.com>
2022-08-02 09:29:16 +0300

    a function that frees dogs.

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,37 +1,19 @@
 #include "dog.h"
-
 #include <stdlib.h>
 
-
-
 /**
+ * free_dog - function to free memory
+ * @d: is pointer to dog_t
  *
- *  * free_dog - function to free memory
- *
- *   * @d: is pointer to dog_t
- *
- *    *
- *
- *     * Return: nothing.
- *
- *     **/
-
-
+ * Return: nothing.
+**/
 
 void free_dog(dog_t *d)
-
 {
-
-	if (d != NULL)
-
-	{
-
-		free(d->name);
-
-		free(d->owner);
-
-		free(d);
-
-	}
-
+if (d != NULL)
+{
+free(d->name);
+free(d->owner);
+free(d);
+}
 }
